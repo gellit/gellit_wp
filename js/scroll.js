9ad5d6d8115475d9d95ca9bdcb9f55c2c7053d9f
gellit <124688078+gellit@users.noreply.github.com>
2023-04-13 12:40:56 +0200

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -8,6 +8,8 @@ let uparrow = document.getElementById("uparrow");
 let distort = document.getElementById("vp2")
 
 window.addEventListener("scroll", function(){
+    let screenWidth = document.body.offsetWidth;
+
     let value = window.scrollY;
     clouds.style.left = value * 0.25 + "px";
     clouds.style.top = value * 0.25 + "px";
@@ -18,26 +20,61 @@ window.addEventListener("scroll", function(){
     if(gellitbead.style.opacity >= 0.85){
         gellitbead.style.opacity = 0.85;
     }
-    if(value < 100){
-        /*gellitbead.style.opacity = 0 + 0.2 / 70 * value;*/
-        uparrow.style.opacity = 0;
+
+    if(screenWidth <= 991){
+        if(value < 50){
+            uparrow.style.opacity = 0;
+        }else{
+            uparrow.style.opacity = (value - 300)/1000;
+            if(uparrow.style.opacity>=0.8){
+                uparrow.style.opacity = 0.8;
+            }
+        }
     }else{
-        /*gellitbead.style.opacity = 1 - 0.2 / 500 * value;*/
-        uparrow.style.opacity = (value - 600)/1000;
-        if(uparrow.style.opacity>=0.8){
-            uparrow.style.opacity = 0.8;
+        if(value < 100){
+            uparrow.style.opacity = 0;
+        }else{
+            uparrow.style.opacity = (value - 600)/1000;
+            if(uparrow.style.opacity>=0.8){
+                uparrow.style.opacity = 0.8;
+            }
         }
     }
+    
 
-    let initClipPath = -51;
-    let clipPath = 0;
-    if(value<(initClipPath*(-1))){
-        vp2.style.clipPath = "circle(45px at 50% " + initClipPath.toString() + "px)"
+    
+    let clipRadius = 0;
+    let clipCoEff = 0;
+    let vpHeight = 0;
+    let initClipPath = 0;
+    let initScrollClip = 0;
+    let scrollRangeClip = 0;
+    if(screenWidth <= 991){
+        clipRadius = 25;
+        clipCoEff = 1.18;
+        vpHeight = 81;
+        initClipPath = -3;
+        initScrollClip = 25;
+        scrollRangeClip = 166;
     }else{
-        clipPath = initClipPath + value/1.5;
-        vp2.style.clipPath = "circle(45px at 50% " + clipPath.toString() + "px)"
+        clipRadius = 45;
+        clipCoEff = 1.5;
+        vpHeight = 134;
+        initClipPath = -10;
+        initScrollClip = 54;
+        scrollRangeClip = 260;
     }
-    console.log(value);
+
+
+    let clipPath = 0;
+    if(value<(initScrollClip)){
+        vp2.style.clipPath = "circle(" + clipRadius.toString() + "px at 50% " + ((-1)*initClipPath).toString() + "px)"
+    }else{
+        let slope = (vpHeight + initClipPath*(-2))/scrollRangeClip;;
+        let section = vpHeight + initClipPath*(-1) - slope * (scrollRangeClip + initScrollClip);
+        clipPath = slope * value + section;
+        vp2.style.clipPath = "circle(" + clipRadius.toString() + "px at 50% " + clipPath.toString() + "px)"
+    }
 
     logo.style.opacity = 1 - 1 / 400 * value;
     
@@ -45,6 +82,8 @@ window.addEventListener("scroll", function(){
     middleground.style.top = value * 0.25 + "px";
     foreground.style.top = value * 0 + "px";
     header.style.top = value * 0.5 + "px";
+
+    
 })
 
 
